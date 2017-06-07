7fbdc3c0cb09ad059467e3fc1b2949b48de8790c
wzmli <wzmichael.li@gmail.com>
2017-06-07 14:41:14 -0400

    8 and above is not working

--- phylogmat.R
+++ phylogmat.R
@@ -1,20 +1,31 @@
 
 library(ape)
+library(Matrix)
 set.seed(101)
-r <- makeNodeLabel(rtree(3))
-plot(r)
+r <- makeNodeLabel(rtree(7))
+# r <- makeNodeLabel(rtree(6))
+# print(r$edge)
+## my previous code failed with rtree(6)
+## trying to reorder/change the phylogenetic tree
+plot(r,show.node.label=TRUE)
 phylo.to.Z2 <- function(r) {
   ntip <- length(r$tip.label)
-  TEmat <- matrix(0.0,ncol=length(r$edge.length),nrow=ntip)
-  colnames(TEmat) <- pos <- r$edge[,2]
+  print(ntip)
+  ee <- r$edge
+  TEmat <- Matrix(0.0,ncol=length(r$edge.length),nrow=ntip)
+  oee <- r$edge[order(r$edge[,1],r$edge[,2]),]
+  pos <- ee[,2]
+  colnames(TEmat) <- opos <- oee[,2]
   rownames(TEmat) <- 1:ntip
-  secondnode <- r$edge[r$edge[,1]==ntip+1,]
+  sn <- r$edge[r$edge[,1]==ntip+1,2]
   for (i in 1:ntip) {
-    ifelse(which(pos==secondnode[2,2])>which(pos==i),
-    TEmat[i,1:which(pos==i)] <- as.numeric((secondnode[1,2]<=pos & pos > i) |pos==i)[1:which(pos==i)],
-    TEmat[i,1:which(pos==i)] <- as.numeric((secondnode[2,2]<=pos & pos != secondnode[1,2])|pos==i)[1:which(pos==i)]
+    ifelse(
+      which(pos==sn[2])>=which(pos==i)
+      , TEmat[i,1:which(opos==i)] <- as.numeric((sn[1]<=opos & opos <= oee[which(opos==i),1]) | opos==i)[1:which(opos==i)]
+      , TEmat[i,1:which(opos==i)] <- as.numeric((sn[2]<=opos & opos <= oee[which(opos==i),1]) | opos==i)[1:which(opos==i)]
     )
   }
+  TEmat <- TEmat[,as.character(r$edge[,2])]
   colnames(TEmat) <- rownames(TEmat) <- NULL
   Z <- t(r$edge.length * t(TEmat))
   return(Z)
@@ -23,7 +34,7 @@ phylo.to.Z2 <- function(r) {
 
 phylo.to.Z <- function(r) {
   ntip <- length(r$tip.label)
-  Z <- matrix(0.0,ncol=length(r$edge.length),nrow=ntip)
+  Z <- Matrix(0.0,ncol=length(r$edge.length),nrow=ntip)
   nodes <- (ntip+1):max(r$edge)
   root <- nodes[!(nodes %in% r$edge[,2])]
   for (i in 1:ntip) {
@@ -36,10 +47,15 @@ phylo.to.Z <- function(r) {
   }
   return(Z)
 }
-phylo.to.Z(r)
+# phylo.to.Z(r)
 
-aa <- phylo.to.Z2(r)
-bb <- phylo.to.Z(r)
+ta<-system.time(aa <- phylo.to.Z2(r))
+
+tb<-system.time(bb <- phylo.to.Z(r))
 # colnames(bb) <- rownames(bb) <- NULL
+aa
+bb
 
 all.equal(unname(aa),bb)
+ta
+tb
